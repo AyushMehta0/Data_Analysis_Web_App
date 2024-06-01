431b26736dc692f7692ddeb1b4a0a4a4bb5b37c5
AyushMehta0 <128117430+AyushMehta0@users.noreply.github.com>
2024-06-01 11:13:37 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#imports 
+import numpy as np
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+
+st.title("Data Analysis")
+st.subheader("Data analysis using Python and Streamlit")
+
+upload = st.file_uploader("Upload Your Dataset (In CSV Format)")
+
+if upload is not None:
+    data = pd.read_csv(upload)
+
+if upload is not None:
+    if st.checkbox("Preview Dataset"):
+        if st.button("Head"):
+            st.write(data.head())
+        if st.button("Tail"):
+            st.write(data.tail())
+
+#Check datatype of each column 
+if upload is not None:
+    if st.checkbox("Datatype of Each Column"):
+        st.text("Datatypes")
+        st.text(data.dtypes)
+
+
+#5. Find Shape of Our Dataset (Number of Rows And Number of Columns)
+if upload is not None:
+    data_shape=st.radio("What Dimension Do You Want To Check?",('Rows',
+                                                                'Columns'))
+    if data_shape=='Rows':
+        st.text("Number of Rows")
+        st.write(data.shape[0])
+    if data_shape=='Columns':
+        st.text("Number of Columns")
+        st.write(data.shape[1])
+
+import matplotlib.pyplot as plt
+
+#6. Find Null Values in The Dataset
+if upload is not None:
+    has_null_values = data.isnull().values.any()
+
+    # Display checkbox and handle user interaction
+    if st.checkbox("Show Heatmap of Missing Values"):
+      # Allow users to select columns (optional)
+      selected_columns = st.multiselect("Select Columns", data.columns)
+      if not selected_columns:
+        selected_columns = data.columns  # Use all columns if none selected
+      null_heatmap_data = data[selected_columns].isnull()
+    
+      if has_null_values:
+        # Generate and display heatmap
+        fig, ax = plt.subplots()  # Using Matplotlib for more customization (optional)
+        sns.heatmap(null_heatmap_data, ax=ax)
+        st.pyplot(fig)
+      else:
+        st.write("No missing values found!")
+            
+
+#7. Find Duplicate Values in the dataset
+if upload is not None:
+    test=data.duplicated().any()
+    if test==True:
+        st.warning("This Dataset Contains Some Duplicate Values")
+        dup=st.selectbox("Do You Want to Remove Duplicate Values?", \
+                         ("Select One","Yes","No"))
+        if dup=="Yes":
+            data=data.drop_duplicates()
+            st.text("Duplicate Values are Removed")
+        if dup=="No":
+            st.text("Ok No Problem")
+    
+#8. Get Overall Statistics
+if upload is not None:
+    if st.checkbox("Summary of The Dataset"):
+        st.write(data.describe(include='all'))
+
+
+#9. About Section
+
+if st.button("About App"):
+    st.text("Built With Streamlit")
+    st.text("Thanks To Streamlit")
+
+
+#10. By
+if st.checkbox("By"):
+    st.success("Ayush Mehta")        
+    
